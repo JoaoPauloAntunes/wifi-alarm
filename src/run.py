b782589b85600f9a8ba518e45f7ab59959a05231
JoaoPauloAntunes <jpantunesdesouza@gmail.com>
2023-02-21 19:21:48 -0300

    feat: Adds .env file

--- src/run.py
+++ src/run.py
@@ -1,6 +1,11 @@
 import uvicorn
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv(".env")
 
 
 # command: cd src && uvicorn main:app --reload
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run("main:app", host=os.environ["HOST_IP_ADDRESS"], port=8000, log_level="info", reload=True)
