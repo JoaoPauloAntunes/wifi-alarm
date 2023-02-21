ad85dc5d2eb4a8d73f96be3997fa4f2abc801341
João Paulo Antunes <48777730+JoaoPauloAntunes@users.noreply.github.com>
2023-02-21 20:39:25 -0300

    Update run.py

--- src/run.py
+++ src/run.py
@@ -6,6 +6,7 @@ import os
 load_dotenv(".env")
 
 
-# command: cd src && uvicorn main:app --reload
+# command: python src/run.py 
+# or: cd src && uvicorn main:app --reload
 if __name__ == "__main__":
     uvicorn.run("main:app", host=os.environ["HOST_IP_ADDRESS"], port=8000, log_level="info", reload=True)
