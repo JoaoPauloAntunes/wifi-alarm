1299c0d8b34b674dac81f652d9111b51b1e3bba4
JoaoPauloAntunes <jpantunesdesouza@gmail.com>
2023-02-22 01:56:52 -0300

    improv: Upgrades environments

--- src/run.py
+++ src/run.py
@@ -7,6 +7,12 @@ load_dotenv(".env")
 
 
 # command: python src/run.py 
-# or: cd src && uvicorn main:app --reload
+# or: cd src && uvicorn main:app --host=localhost --port=8000 log_level=info
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=os.environ["HOST_IP_ADDRESS"], port=8000, log_level="info", reload=True)
+    uvicorn.run(
+        "main:app", 
+        host=os.environ["HOST_IP_ADDRESS"], 
+        port=8000, 
+        log_level="info", 
+        reload=os.getenv("APP_RELOAD", 'False').lower() in ('true', '1', 't')
+    )
