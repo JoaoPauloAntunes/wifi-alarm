1821ded62a9b8c65dd4ac67be87269e334d6f7b5
JoaoPauloAntunes <jpantunesdesouza@gmail.com>
2023-02-26 19:47:23 -0300

    improv: Configures to requesting the alarm ring when button is pressed

--- esp32/main.py
+++ esp32/main.py
@@ -1,8 +1,22 @@
 print(" ----------------------------------main.py---------------------------------- ")
 import urequests
+import machine
+import time
 
 
-base_url = "http://192.168.1.246:8000" # PC
-base_url = "http://192.168.1.240:8000" # Smartphone Novo
-base_url = "http://192.168.1.31:8000" # Smartphone Velho
-print(urequests.post(f"{base_url}/alarm"))
+button = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
+while True:
+    first = button.value()
+    time.sleep(0.01) # debounce time
+    second = button.value()
+    if first and not second:
+        print('Button pressed!')
+
+        print('Requesting the alarm ring...')
+        base_url = "http://192.168.1.246:8000" # PC
+        base_url = "http://192.168.1.240:8000" # Smartphone Novo
+        base_url = "http://192.168.1.31:8000" # Smartphone Velho
+        print(urequests.post(f"{base_url}/alarm"))
+    elif not first and second:
+        print('Button released!')
+  
